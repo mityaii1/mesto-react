10af00416192a61419d80eb75bf2b26d0693e6ff
mityaii1 <63926278+mityaii1@users.noreply.github.com>
2020-09-27 00:54:35 +0500

    Проект №11

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
-import api from '../utils/api'
+import api from '../utils/Api'
 import PopupWithForm from './PopupWithForm';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
