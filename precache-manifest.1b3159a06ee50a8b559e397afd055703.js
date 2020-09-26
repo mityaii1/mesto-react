b8222b34d78177999ddbf1a03c9dce54a6e16dd3
mityaii1 <63926278+mityaii1@users.noreply.github.com>
2020-09-26 23:56:02 +0500

    Updates

---
+++ precache-manifest.1b3159a06ee50a8b559e397afd055703.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac0666d8579d3a480861b50765e6b4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af7103f7789138b3ecf",
+    "url": "/static/css/main.488bd96a.chunk.css"
+  },
+  {
+    "revision": "6cb719134dcea849c260",
+    "url": "/static/js/2.9b140ed9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b140ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af7103f7789138b3ecf",
+    "url": "/static/js/main.ab8f7417.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "f6cf0a0bc5fce3307e2c426eb14eb752",
+    "url": "/static/media/Inter-Medium.f6cf0a0b.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/add-button.b2fce4ac.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "/static/media/close-Icon.5df32a06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/edit-button.6bfdb354.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "/static/media/header-logo.c6f11019.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "/static/media/like-active.5bbb05f7.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "/static/media/like.83a46f45.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "/static/media/remove.d8078343.svg"
+  }
+]);
